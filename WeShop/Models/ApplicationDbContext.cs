7f89f791f91bfba99fb2b2a83768cd7a01034a77
rares-munteanu <rares.munteanucni@yahoo.com>
2021-01-10 17:48:34 +0200

    Configured Database

---
+++ WeShop/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WeShop.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //public DbSet<Custome Type { get; set; }
+
+        public ApplicationDbContext()
+            : base("WeShopConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
